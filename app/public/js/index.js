caa88095c8a789c7e6fc85bda1a98de0018b90b5
cajharsh <cajharsh@iu.edu>
2021-09-20 17:09:26 -0700

    Update index.js

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -11,26 +11,22 @@ const Profile = {
             .format('DD MMM YYYY')
         }
     },
-
-    methods: {
-        fetchUserData() {
-            console.log("A");
-            fetch('https://randomuser.me/api/')
-            .then(response => response.json())
-            .then((parsedJson) => {
-                console.log(parsedJson);
-                this.person = parsedJson.results[0]
-                console.log("C");
-            })
-                .catch( err => {
-                    console.error(err)
-            })
     
-            console.log("B");
-        }
-    },
     created() {
-       this.fetchUserData();
+        console.log("A");
+
+        fetch('https://randomuser.me/api/')
+        .then(response => response.json())
+        .then((parsedJson) => {
+            console.log(parsedJson);
+            this.person = parsedJson.results[0]
+            console.log("C");
+        })
+            .catch( err => {
+                console.error(err)
+        })
+
+            console.log("B");
 
     }
 }
