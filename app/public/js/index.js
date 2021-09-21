d53465bb29bbb0b3a3d614c90ab127dcbce29237
cajharsh <cajharsh@iu.edu>
2021-09-20 17:04:27 -0700

    prettybirthday

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -4,21 +4,33 @@ const Profile = {
             "person": {},
         }
     },
+
+    computed: {
+        prettyBirthday() {
+            return dayjs(this.person.dob.date)
+            .format('DD MMM YYYY')
+        }
+    },
+
+    methods: {
+        fetchUserData() {
+            console.log("A");
+            fetch('https://randomuser.me/api/')
+            .then(response => response.json())
+            .then((parsedJson) => {
+                console.log(parsedJson);
+                this.person = parsedJson.results[0]
+                console.log("C");
+            })
+                .catch( err => {
+                    console.error(err)
+            })
+    
+                console.log("B");
+        }
+    },
     created() {
-        console.log("A");
-
-        fetch('https://randomuser.me/api/')
-        .then(response => response.json())
-        .then((parsedJson) => {
-            console.log(parsedJson);
-            this.person = parsedJson.results[0]
-            console.log("C");
-        })
-            .catch( err => {
-                console.error(err)
-        })
-
-            console.log("B");
+       this.fetchUserData();
 
     }
 }
