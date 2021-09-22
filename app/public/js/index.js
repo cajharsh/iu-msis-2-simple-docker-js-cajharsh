c3e4d72b8a1921df20cb1e61c4625ae424d98c01
cajharsh <cajharsh@iu.edu>
2021-09-22 00:05:28 -0700

    Refresh button

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -10,22 +10,22 @@ const Profile = {
             .format('DD MMM YYYY')
         }
     },
-    created() {
-        console.log("A");
-
-        fetch('https://randomuser.me/api/')
-        .then(response => response.json())
-        .then((parsedJson) => {
-            console.log(parsedJson);
-            this.person = parsedJson.results[0]
-            console.log("C");
-        })
+    methods: {
+        fetchUserData() {
+            fetch('https://randomuser.me/api/')
+            .then(response => response.json())
+            .then((parsedJson) => {
+                console.log(parsedJson);
+                this.person = parsedJson.results[0]
+                console.log("C");
+            })
             .catch( err => {
-                console.error(err)
-        })
-
-            console.log("B");
-
+                console.error(err);
+            });
+        }
+    },
+    created() {
+        this.fetchUserData();
     }
 }
 Vue.createApp(Profile).mount('#profileApp');
