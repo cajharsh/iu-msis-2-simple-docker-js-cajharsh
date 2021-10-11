5667e11bf210d0cc91633ea27d20ae2a7e0e17dc
cajharsh <cajharsh@iu.edu>
2021-10-11 10:19:19 -0400

    connecting to db

---
+++ app/data/schema.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE IF NOT EXISTS msisdb;
+USE msisdb;
+
+DROP TABLE IF EXISTS student;
+CREATE TABLE student (
+	id int PRIMARY KEY AUTO_INCREMENT ,
+    username varchar(24) UNIQUE NOT NULL,
+    name varchar(48)
+);
+
+INSERT INTO student (id, username, name) VALUES 
+(1, 'tomgreg', 'Tom Gregory'),
+(2, 'beth1', 'Beth Barnhart'),
+(3, 'bipin', 'Prof. Prabhakar');
--- app/public/api/book/index.php
+++ app/public/api/book/index.php
@@ -2,27 +2,32 @@
 // require 'common.php';
 require 'class/DbConnection.php';
 
-
 // Step 1: Get a datase connection from our helper class
 $db = DbConnection::getConnection();
 
 // Step 2: Create & run the query
-$sql = 'SELECT * FROM student';
+$sql = 'SELECT * FROM book';
 $vars = [];
 
+//Wednesday Class
+
 // if (isset($_GET['guid'])) {
-//   // This is an example of a parameterized query
-//   $sql = 'SELECT * FROM Patient WHERE patientGuid = ?';
-//   $vars = [ $_GET['guid'] ];
-// }
+   //This is an example of a parameterized query
+ // $sql = 'SELECT * FROM offer WHERE studentID = ?';
+
+ //NOT THIS WAY
+ //$sql = 'SELECT * FROM offer WHERE studentID = ' . $_GET['studentID']
+
+ // $vars = [ $_GET['guid'] ];
+ //}
 
 $stmt = $db->prepare($sql);
 $stmt->execute($vars);
 
-$patients = $stmt->fetchAll();
+$books = $stmt->fetchAll();
 
 // Step 3: Convert to JSON
-$json = json_encode($studentss, JSON_PRETTY_PRINT);
+$json = json_encode($books, JSON_PRETTY_PRINT);
 
 // Step 4: Output
 header('Content-Type: application/json');
