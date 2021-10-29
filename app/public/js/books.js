90d0bd454c2df0d40f8ffd43053d8c1b4b0c7f4d
cajharsh <cajharsh@iu.edu>
2021-10-29 16:59:17 -0400

    Update books.js

--- app/public/js/books.js
+++ app/public/js/books.js
@@ -36,10 +36,34 @@ const SomeApp = {
         },
         postNewBook(evt) {
             //this.bookForm.bookId = this.selectedBook.id;
-            console.log("Posting:", this.bookForm);
+            console.log("Creating:", this.bookForm);
             //alert("Posting!");
 
             fetch('api/books/create.php',{
+                method:'POST',
+                body: JSON.stringify(this.bookForm),
+                headers: {
+                    "Content-Type": "application/json; charset=utf-8"
+                }
+            })
+           .then(response => response.json())
+           .then(json => {
+               console.log("Returned from post:", json);
+               //TODO: test a result was returned!
+               this.books = json;
+
+               //reset the form
+               this.resetBookForm = {};
+           }); 
+        },
+        postEditBook(evt) {
+            this.bookForm.bookId = this.selectedBook.id;
+            this.offerForm.id = this.selectedBook.id;
+
+            console.log("Updating:", this.bookForm);
+            //alert("Posting!");
+
+            fetch('api/books/update.php',{
                 method:'POST',
                 body: JSON.stringify(this.bookForm),
                 headers: {
